0b55bf5380784b0642ec8b1071ce778f313aa8ff
Brendan McShane <brendanmcs7@gmail.com>
2019-08-24 21:43:11 -0400

    TV view

--- RPI TV/App.xaml.cs
+++ RPI TV/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,6 +29,7 @@ namespace RPI_TV
         /// </summary>
         public App()
         {
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
--- RPI TV/MainPage.xaml.cs
+++ RPI TV/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FFmpegInterop;
+using Windows.Media.Core;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,17 +25,67 @@ namespace RPI_TV
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private FFmpegInteropMSS FFmpegMSS;
         public MainPage()
         {
             this.InitializeComponent();
-        }
 
+            //stream:
+            Settings.Source = "rtmp://192.168.1.51:1935/live/5c33d398e4b02b6251941977_0";
+            Stream();
+        }
+        private void Stream()
+        {
+            try
+            {
+                PropertySet options = new PropertySet();
+                options.Add("rtsp_flags", "prefer_udp");
+
+                Player.Stop();
+                Settings.RefreshSettings();
+                FFmpegMSS = FFmpegInteropMSS.CreateFFmpegInteropMSSFromUri(Settings.Source, Settings.AudioDecode, true, options);
+                if (FFmpegMSS != null)
+                {
+                    MediaStreamSource mss = FFmpegMSS.GetMediaStreamSource();
+                    if (mss != null)
+                    {
+                        Player.SetMediaStreamSource(mss);
+
+                    }
+                    else
+                    {
+                        Errors();
+                        return;
+                    }
+                }
+                else
+                {
+                    Errors();
+                    return;
+                }
+            }
+            catch
+            {
+                Errors();
+                return;
+            }
+        }
+        private void MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            Errors();
+        }
+        private void Errors()
+        {
+            Settings.ErrorCount++;
+        }
     }
     public class Settings
     {
-        public static string Source { get; }
+        public static string Source { get; set; }
         public static string Name { get; set; }
-        public static void GetSource()
+        public static bool AudioDecode { get; set; }
+        public static int ErrorCount { get; set; }
+        public static void RefreshSettings()
         {
 
         }
