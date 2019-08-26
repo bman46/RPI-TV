40e40ef719021b3550132e836cce9b8d0927eed7
Brendan McShane <brendanmcs7@gmail.com>
2019-08-26 11:44:31 -0400

    misc

--- Controller/Pages/Channels.cshtml.cs
+++ Controller/Pages/Channels.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -13,7 +14,33 @@ namespace Controller.Pages
     {
         public void OnGet()
         {
-
         }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+                using (var db = new Model.DeviceChannels())
+                {
+                    db.Channels.Add(new Model.Channel { ChannelName = ChannelNameIn, Url = URLIn });
+                    db.SaveChanges();
+                }
+
+                return RedirectToPage("Channels");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+        [BindProperty]
+        [Required]
+        [MinLength(2)]
+        public string ChannelNameIn { get; set; }
+
+        [BindProperty]
+        [Required]
+        [MinLength(6)]
+        public string URLIn { get; set; }
     }
 }
